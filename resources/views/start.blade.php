91d34285f92936672f5592b894fadccf5b222cb1
unknown <artsawatzki@gmail.com>
2019-11-24 10:47:05 +0100

    My first vue component

---
+++ resources/views/start.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>OK</h1>
+    <my-vue-component></my-vue-component>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('start');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
